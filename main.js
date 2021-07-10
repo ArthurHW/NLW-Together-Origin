12d521c4b96f91731a0a234d0d9f83fa928f552c
Arthur Henrique Wiebusch <arturh07@gmail.com>
2021-07-10 15:14:40 -0300

    Quinta aula concluida

--- main.js
+++ main.js
@@ -35,7 +35,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 //  Rolagem suave
@@ -69,7 +75,34 @@ function backToTop() {
   }
 }
 
+// Menu ativo conforme a seção visível na página
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + window.innerHeight / 2
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+// Quando rolar
 window.addEventListener('scroll', () => {
   addShadowToHeader()
   backToTop()
+  activateMenuAtCurrentSection()
 })
